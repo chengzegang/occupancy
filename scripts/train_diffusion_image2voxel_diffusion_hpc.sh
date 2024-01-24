ceb0d6c7cfa0b443b2e59efd10153727a5751432
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2024-01-23 20:28:33 -0500

    update

---
+++ scripts/train_diffusion_image2voxel_diffusion_hpc.sh
@@ -0,0 +1,4 @@
+export HF_HOME=/scratch/zc2309/.cache/huggingface
+python -m occupancy.pipelines.train --model diffusion3d --data-dir /nuscenes/nuscenes \
+    --cache-dir .cache/nuscenes --batch-size 8 --num-workers 16 --device cuda \
+    --dtype bf16 --model-config config/diffusion_image2voxel.yml --grad-accum 4 --save-every 100 --weight-decay 0.1
