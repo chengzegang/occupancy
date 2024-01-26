0642e306629c60424b40b51e629075d3d1233aaf
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2024-01-25 23:04:57 -0500

    update

--- occupancy/pipelines/panoramic2voxel.py
+++ occupancy/pipelines/panoramic2voxel.py
@@ -347,7 +347,7 @@ class MultiViewImageToVoxelModel(nn.Module):
         self.radius_channels = radius_channels
         self.encoder = UnetEncoder2d(4, self.hidden_size, 512, 2, 2, out_norm=False)
         self.grid_embeds = nn.Conv3d(3, self.hidden_size, 3, padding=1)
-        self.transformer = Transformer(self.hidden_size, 36, self.hidden_size // 128, 128)
+        self.transformer = Transformer(self.hidden_size, 32, self.hidden_size // 128, 128)
         self.decoder = UnetConditionalAttentionDecoderWithoutShortcut3d(
             out_channels, self.hidden_size, self.hidden_size, 512, 2, 1, 128
         )
