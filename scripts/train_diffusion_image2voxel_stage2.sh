fce665b545a85d97682d4acf36be80f98782feb0
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2024-01-29 21:59:13 -0500

    update

--- scripts/train_diffusion_image2voxel_stage2.sh
+++ scripts/train_diffusion_image2voxel_stage2.sh
@@ -1,6 +1,6 @@
 export WANDB_MODE=disabled
 python -m occupancy.pipelines.train --model panoramic2voxel --data-dir '/mnt/f/datasets/nuscenes/' \
-    --cache-dir .cache/nuscenes --batch-size 1 --num-workers 8 --device cuda \
-    --dtype bf16 --model-config config/diffusion_image2voxel.yml --grad-accum 1 --save-every 100 --weight-decay 1.0e-5 --lr 1.0e-4 \
+    --cache-dir .cache/nuscenes --batch-size 4 --num-workers 8 --device cuda \
+    --dtype bf16 --model-config config/diffusion_image2voxel.yml --grad-accum 32 --save-every 100 --weight-decay 1.0e-1 --lr 1.0e-4 \
     --warmup-steps 10 --num-classes 18
     
