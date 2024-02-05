3199c944d9122c3ebf84dd8ccb5f62a23525068e
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2024-02-05 10:49:27 -0500

    update

--- scripts/train_diffusion_image2voxel_stage2_hpc1.sh
+++ scripts/train_diffusion_image2voxel_stage2_hpc1.sh
@@ -1,4 +1,5 @@
 export HF_HOME=/scratch/zc2309/.cache/huggingface
+pip install nuscenes-devkit -U
 python -m occupancy.pipelines.train --model panoramic2voxel --data-dir /nuscenes/nuscenes \
     --cache-dir .cache/nuscenes --batch-size 4 --num-workers 16 --device cuda \
     --dtype fp32 --model-config config/diffusion_image2voxel.yml --grad-accum 16 --save-every 100 --weight-decay 1.0e-5 \
